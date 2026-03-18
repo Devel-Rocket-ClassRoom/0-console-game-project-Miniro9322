a1a42028274227bc4474295441da21baf8c48042
Miniro9322 <justly2000@naver.com>
2026-03-18 16:15:42 +0900

    레포 생성

---
+++ console-game-framework/MyGame/BomberMan.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Framework.Engine;
+
+class BomberMan : GameApp
+{
+    public BomberMan(int width, int height) : base(width, height)
+    {
+
+    }
+
+    protected override void Draw()
+    {
+
+    }
+
+    protected override void Initialize()
+    {
+
+    }
+
+    protected override void Update(float deltaTime)
+    {
+
+    }
+}
